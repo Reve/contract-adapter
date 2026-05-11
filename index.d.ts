9b393f054092a8fac2af15ffd0637e89e0497b1b
emanuel.amuza <emanuel.amuza21@gmail.com>
2026-05-12 00:27:18 +0300

    Add exclude array to serialize and unserialize

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module 'adaptr' {
     export default class Adaptr {
         constructor(key: string, schema: object);
 
-        serialize(data: object): object;
-        unserialize(data: object): object;
+        serialize(data: object, exclude?: string[]): object;
+        unserialize(data: object, exclude?: string[]): object;
     }
 }
